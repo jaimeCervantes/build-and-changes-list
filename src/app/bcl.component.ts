569f70630006bb14bac3b6c7c0cc8affa84a56e9
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-01-22 13:25:06 -0600

    refactor(css): configuring scss styles and adding font-awesome for icons

--- src/app/bcl.component.ts
+++ src/app/bcl.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bcl-root',
   templateUrl: './bcl.component.html',
-  styleUrls: ['./bcl.component.css']
+  styleUrls: ['./bcl.component.scss']
 })
 export class BclComponent {
   title = 'bcl works!';
